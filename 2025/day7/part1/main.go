29dfa4a3f618c4f7a7e271b92ba60ac4d1db6ea0
Yarden Shoham <git@yardenshoham.com>
2025-12-07 09:27:01 +0200

    Add 2025 day 7

---
+++ 2025/day7/part1/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"os"
+)
+
+func beam(grid map[image.Point]byte, memo map[image.Point]int, x, y int) int {
+	curr, ok := grid[image.Pt(x, y)]
+	if !ok || curr == '|' {
+		return 0
+	}
+	if m, ok := memo[image.Pt(x, y)]; ok {
+		return m
+	}
+	if curr == '^' {
+		res := 1 + beam(grid, memo, x-1, y+1) + beam(grid, memo, x+1, y+1)
+		memo[image.Pt(x, y)] = res
+		return res
+	}
+	res := beam(grid, memo, x, y+1)
+	memo[image.Pt(x, y)] = res
+	grid[image.Pt(x, y)] = '|'
+	return res
+}
+
+func main() {
+	input, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	grid := map[image.Point]byte{}
+
+	var sX, sY int
+	for y, line := range bytes.Fields(input) {
+		for x, b := range line {
+			grid[image.Pt(x, y)] = b
+			if b == 'S' {
+				sX = x
+				sY = y
+			}
+		}
+	}
+	memo := map[image.Point]int{}
+	fmt.Println(beam(grid, memo, sX, sY))
+}
